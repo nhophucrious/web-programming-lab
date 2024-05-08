4ec25dfda4032b825eda8b84c6975c5523672b70
Phuc Nguyen Nho Gia <nngiaphuc0101@gmail.com>
2024-05-08 19:52:30 +0700

    fix: minor bug fix (prolly final too

--- views/course_detail.php
+++ views/course_detail.php
@@ -4,6 +4,10 @@ require_once 'includes/header.php';
 
 <div class="container" style="min-height: 100vh">
     <div class="row pt-5">
+        <a href="/web-programming-lab/" class="my-button mr-2">Go back home</a>
+        <a href="/web-programming-lab/course-paginate" class="my-button-filled">Or explore more products</a>
+    </div>
+    <div class="row pt-5">
         <!-- image on the left -->
         <div class="col-md-6">
             <img src="<?php echo $course['url']; ?>" alt="<?php echo $course['course_name']; ?>" class="img-fluid">
